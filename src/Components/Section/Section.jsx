664fc325de8d4c6ce745ebb0f9c33c4aefc377ab
logeshloki585 <logeshtv21@gmail.com>
2021-10-15 12:02:59 +0530

    finished

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -7,7 +7,7 @@ export const Section = () => {
             <div className="h-44">
 
             </div>
-            <div class="w-full max-w-xs mx-24">
+            <div class="w-full max-w-xs mx-8 lg:mx-24 ">
             <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
